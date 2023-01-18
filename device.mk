5971da885aedc53396f4e6a8272b58360eeb482f
tibixh <tibi.nemeth12@gmail.com>
2023-01-18 15:05:40 +0100

    lisa: build mlipay this seems to be required for the media HAL's to work

--- device.mk
+++ device.mk
@@ -29,5 +29,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti
 
+# Mlipay
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.mlipay@1.1.vendor \
+    vendor.xiaomi.hardware.mtdservice@1.0.vendor
+
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/lisa/lisa-vendor.mk)
